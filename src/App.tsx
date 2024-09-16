ccde61037766be26ee8d65efaa93ff15c82cec54
Luciano Ramello <luchosr@gmail.com>
2024-09-16 11:15:00 +0200

    feat: implements environment variables usage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import useWeather from './hooks/useWeather';
 
 function App() {
   const { fetchWeather } = useWeather();
+
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -3,7 +3,7 @@ import { SearchType } from '../types';
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
-    const appId = 'f0c2e56a92ab26c9bd75d799b391b274';
+    const appId = import.meta.env.VITE_API_KEY;
 
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
