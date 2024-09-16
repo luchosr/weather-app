2dbaa9da51665b62313454afcd439ef3f85079a7
Luciano Ramello <luchosr@gmail.com>
2024-09-16 11:03:24 +0200

    feat: implements geo localization data fetching

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,19 @@
+import axios from 'axios';
+import { SearchType } from '../types';
+
 export default function useWeather() {
-  const fetchWeather = () => {
-    console.log('Fetching weather...');
+  const fetchWeather = async (search: SearchType) => {
+    const appId = 'f0c2e56a92ab26c9bd75d799b391b274';
+
+    try {
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
+
+      const { data } = await axios.get(geoUrl);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return {
