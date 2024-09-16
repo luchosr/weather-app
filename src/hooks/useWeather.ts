05cb60426559665869329003729a52777c61f90f
Luciano Ramello <luchosr@gmail.com>
2024-09-16 13:40:54 +0200

    feat: implement type cast, type guards and zod

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,31 @@
+import { Weather } from './../types/index';
 import axios from 'axios';
+import { z } from 'zod';
 import { SearchType } from '../types';
+
+// ----------------TYPE GUARD O ASSERTION----------------
+// function isWeatherResponse(weather : unknown) : weather is Weather {
+//     return (
+//         Boolean(weather) &&
+//         typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number'
+//     )
+// }
+
+// ----------------Zod----------------
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+
+type Weather = z.infer<typeof Weather>;
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -14,8 +40,28 @@ export default function useWeather() {
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
+      // ------------ Castear el type -------------
+      // const {data: weatherResult} = await axios<Weather>(weatherUrl)
+      // console.log(weatherResult.temp)
+      // console.log(weatherResult.name)
+
+      // ------------Type Guards--------------
+      // const {data: weatherResult} = await axios(weatherUrl)
+      // const result = isWeatherResponse(weatherResult)
+      // if(result) {
+      //     console.log(weatherResult.name)
+      // } else {
+      //     console.log('Respuesta mal formada')
+      // }
+
+      // ------------Zod----------------
       const { data: weatherResult } = await axios.get(weatherUrl);
-      console.log(weatherResult);
+      const result = Weather.safeParse(weatherResult);
+
+      if (result.success) {
+        console.log(result.data.name);
+        console.log(result.data.main.temp);
+      }
     } catch (error) {
       console.log(error);
     }
