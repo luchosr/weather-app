2a099de93495c043251f1c171c7ac2e3b098c62f
Luciano Ramello <luchosr@gmail.com>
2024-09-16 16:55:08 +0200

    feat: implements Weather Detail on app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import styles from './App.module.css';
 import Form from './components/Form/Form';
+import WeatherDetail from './components/WeatherDetail/WeatherDetail';
 import useWeather from './hooks/useWeather';
 
 function App() {
-  const { fetchWeather } = useWeather();
+  const { weather, fetchWeather } = useWeather();
 
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
         <Form fetchWeather={fetchWeather} />
-        <p>2</p>
+        <WeatherDetail />
       </div>
     </>
   );
