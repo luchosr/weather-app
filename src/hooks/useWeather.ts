9717476d1ea4115dc81615a8ac0f4fedb2057a9a
Luciano Ramello <luchosr@gmail.com>
2024-09-16 16:54:13 +0200

    feat: adds weather state

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 // import { object, string, number, Output, parse } from 'valibot';
 import { z } from 'zod';
 import { SearchType } from '../types';
+import { useState } from 'react';
 
 // ----------------TYPE GUARD O ASSERTION----------------
 // function isWeatherResponse(weather : unknown) : weather is Weather {
@@ -16,16 +17,6 @@ import { SearchType } from '../types';
 //     )
 // }
 
-// ----------------Zod----------------
-const Weather = z.object({
-  name: z.string(),
-  main: z.object({
-    temp: z.number(),
-    temp_max: z.number(),
-    temp_min: z.number(),
-  }),
-});
-export type Weather = z.infer<typeof Weather>;
 // ----------------Valibot----------------
 
 // const WeatherSchema = object({
@@ -40,9 +31,25 @@ export type Weather = z.infer<typeof Weather>;
 // type Weather = Output<typeof WeatherSchema>;
 
 // ----------------Zod----------------
-// type Weather = z.infer<typeof Weather>;
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+export type Weather = z.infer<typeof Weather>;
 
 export default function useWeather() {
+  const [weather, setWeather] = useState<Weather>({
+    name: '',
+    main: {
+      temp: 0,
+      temp_max: 0,
+      temp_min: 0,
+    },
+  });
   const fetchWeather = async (search: SearchType) => {
     const appId = import.meta.env.VITE_API_KEY;
 
@@ -82,8 +89,7 @@ export default function useWeather() {
       const result = Weather.safeParse(weatherResult);
 
       if (result.success) {
-        console.log(result.data.name);
-        console.log(result.data.main.temp);
+        setWeather(result.data);
       }
     } catch (error) {
       console.log(error);
@@ -91,6 +97,7 @@ export default function useWeather() {
   };
 
   return {
+    weather,
     fetchWeather,
   };
 }
