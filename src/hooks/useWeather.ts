78a1a91280f78d18379ad676529fa049cc4204b2
Luciano Ramello <luchosr@gmail.com>
2024-09-17 16:56:07 +0200

    feat: creates use loading state on useWeather hooks

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -50,9 +50,10 @@ export default function useWeather() {
       temp_min: 0,
     },
   });
+  const [loading, setLoading] = useState(false);
   const fetchWeather = async (search: SearchType) => {
     const appId = import.meta.env.VITE_API_KEY;
-
+    setLoading(true);
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
 
@@ -93,6 +94,8 @@ export default function useWeather() {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -100,6 +103,7 @@ export default function useWeather() {
 
   return {
     weather,
+    loading,
     fetchWeather,
     hasWeatherData,
   };
