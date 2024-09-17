faa1f7bb06c557ce5a2fbf8474f9fd8954bdae3b
Luciano Ramello <luchosr@gmail.com>
2024-09-17 17:31:49 +0200

    feat: implement notFound state for error handling

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -41,23 +41,32 @@ const Weather = z.object({
 });
 export type Weather = z.infer<typeof Weather>;
 
+const initialState = {
+  name: '',
+  main: {
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+  },
+};
 export default function useWeather() {
-  const [weather, setWeather] = useState<Weather>({
-    name: '',
-    main: {
-      temp: 0,
-      temp_max: 0,
-      temp_min: 0,
-    },
-  });
+  const [weather, setWeather] = useState<Weather>(initialState);
   const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+
   const fetchWeather = async (search: SearchType) => {
     const appId = import.meta.env.VITE_API_KEY;
     setLoading(true);
+    setWeather(initialState);
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
 
       const { data } = await axios.get(geoUrl);
+
+      if (!data[0]) {
+        setNotFound(true);
+        return;
+      }
 
       const { lat, lon } = data[0];
 
@@ -104,6 +113,7 @@ export default function useWeather() {
   return {
     weather,
     loading,
+    notFound,
     fetchWeather,
     hasWeatherData,
   };
