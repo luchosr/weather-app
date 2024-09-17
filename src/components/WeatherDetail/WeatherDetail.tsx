4f58f1419ee8527563f1bf79384a5e0668a08e9f
Luciano Ramello <luchosr@gmail.com>
2024-09-17 13:47:19 +0200

    feat: adds WeatherDetail component

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,3 +1,23 @@
-export default function WeatherDetail() {
-  return <div>WeatherDetail</div>;
+import { formatTemperature } from '../../helpers';
+import { Weather } from '../../hooks/useWeather';
+
+type WeatherDetailProps = {
+  weather: Weather;
+};
+
+export default function WeatherDetail({ weather }: WeatherDetailProps) {
+  return (
+    <div>
+      <h2>Clima de : {weather.name}</h2>
+      <p>Temperatura: {formatTemperature(weather.main.temp)} °C</p>
+      <div className="">
+        <p>
+          Min: <span>{formatTemperature(weather.main.temp_min)} °C</span>
+        </p>
+        <p>
+          Max: <span>{formatTemperature(weather.main.temp_max)} °C</span>
+        </p>
+      </div>
+    </div>
+  );
 }
