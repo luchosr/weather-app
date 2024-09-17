696c39c64702e859ee3c966317298bb185ca5df1
Luciano Ramello <luchosr@gmail.com>
2024-09-17 15:27:35 +0200

    style: implements weather detail CSS classes

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,16 +1,20 @@
 import { formatTemperature } from '../../helpers';
 import { Weather } from '../../hooks/useWeather';
 
+import styles from './WeatherDetail.module.css';
+
 type WeatherDetailProps = {
   weather: Weather;
 };
 
 export default function WeatherDetail({ weather }: WeatherDetailProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Clima de : {weather.name}</h2>
-      <p>Temperatura: {formatTemperature(weather.main.temp)} °C</p>
-      <div className="">
+      <p className={styles.current}>
+        Temperatura: {formatTemperature(weather.main.temp)} °C
+      </p>
+      <div className={styles.temperatures}>
         <p>
           Min: <span>{formatTemperature(weather.main.temp_min)} °C</span>
         </p>
