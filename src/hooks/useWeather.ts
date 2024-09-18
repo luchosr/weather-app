95378b20b3ec02f04be2dc6ea576cb45b3f107c7
Luciano Ramello <luchosr@gmail.com>
2024-09-18 09:13:00 +0200

    fix: updates geo url

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -59,7 +59,7 @@ export default function useWeather() {
     setLoading(true);
     setWeather(initialState);
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search?.city},${search?.country}&appid=${appId}`;
 
       const { data } = await axios.get(geoUrl);
 
